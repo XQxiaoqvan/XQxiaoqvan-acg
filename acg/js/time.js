b8c9f375ab3bcea2ca687018dcb8c7bb739741ab
XQxiaoqvan <xiaoqvandd@qq.com>
2023-11-01 12:07:54 +0800

    更新

--- acg/js/time.js
+++ acg/js/time.js
@@ -1,18 +1,26 @@
-now = new Date(), hour = now.getHours()
-if (hour < 6) {
-    var hello = "凌晨好";
-} else if (hour < 9) {
-    var hello = "早上好";
-} else if (hour < 12) {
-    var hello = "上午好";
-} else if (hour < 14) {
-    var hello = "中午好";
-} else if (hour < 17) {
-    var hello = "下午好";
-} else if (hour < 19) {
-    var hello = "傍晚好";
-} else if (hour < 22) {
-    var hello = "晚上好";
-} else {
-    var hello = "夜深了";
+window.onload = function() {
+    var now = new Date();
+    var hour = now.getHours();
+    var hello;
+
+    if (hour < 6) {
+        hello = "凌晨好";
+    } else if (hour < 9) {
+        hello = "早上好";
+    } else if (hour < 12) {
+        hello = "上午好";
+    } else if (hour < 14) {
+        hello = "中午好";
+    } else if (hour < 17) {
+        hello = "下午好";
+    } else if (hour < 19) {
+        hello = "傍晚好";
+    } else if (hour < 22) {
+        hello = "晚上好";
+    } else {
+        hello = "夜深了";
+    }
+
+    // 在页面中显示问候语
+    document.getElementById("greeting").innerHTML = hello;
 }
